63982df97885fb2eb0e1b90698ec1103868c2282
MJZTEC2021 <mjztec2007@verizon.net>
2021-12-02 19:41:42 -0500

    Added player computer selection

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 // let playerSelection = prompt('Type your choice of rock, paper or scissors.');
 
 let playerSelection = 'rock';
+//let computerSelection = computerPlay();
+
+function computerPlay() {
+  const rps = ['rock', 'paper', 'scissors'];
+let random = Math.floor(Math.random() * rps.length);
+return rps[random];
+}
+
 let computerSelection = computerPlay();
 
-function computerPlay() {}
+function playGame(playerSelection, computerSelection) {
+if (playerSelection === computerSelection) {
+  console.log("It's a tie");
+} else if (
+  (computerSelection === 'rock' && playerSelection === 'scissors') ||
+  (computerSelection === 'paper' && playerSelection === 'rock') ||
+  (computerSelection === 'scissors' && playerSelection === 'paper')
+){
+ console.log ("You loose!") 
 
-function playGame(playerSelection, computerSelection) {}
+}
 
 playGame(playerSelection, computerSelection);
+playerSelection
