f4e70d875980a1085093f1c61e084be89423647b
MJZTEC2021 <mjztec2007@verizon.net>
2021-12-05 14:33:37 -0500

    working on code

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // let playerSelection = prompt('Type your choice of rock, paper or scissors.');
-
 let playerSelection = 'rock';
+
 //let computerSelection = computerPlay();
+let computerSelection = computerPlay();
+
 
 function computerPlay() {
   const rps = ['rock', 'paper', 'scissors'];
@@ -9,23 +11,23 @@ let random = Math.floor(Math.random() * rps.length);
 return rps[random];
 }
 
-let computerSelection = computerPlay();
+
 
 function playGame(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     console.log("It's a tie");
 
-} else if {
+  else (computerSelection > playerSelection)
   (
   (computerSelection === 'rock' && playerSelection === 'scissors') ||
-  //(computerSelection === 'scissors' && playerSelection === 'paper') ||
-  //(computerSelection === 'paper' && playerSelection === 'rock')) {
+  (computerSelection === 'scissors' && playerSelection === 'paper') ||
+  (computerSelection === 'paper' && playerSelection === 'rock')) 
   
-//){ console.log ("You loose!) ${computerSelection} beats ${playerSelection}.");
-//} else {
-//  console.log("You win! ${playerSelection"} beats ${computerSelection}.);
-// }
-//}
+console.log ("You loose!");
+} else if {
+console.log("You win! $(playerSelection"); beats $(computerSelection));
+}
+}
 
 
 
