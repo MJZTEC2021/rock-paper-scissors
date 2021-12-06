893d9cc21f81d1f3b57eb2be198ef604f614508c
MJZTEC2021 <mjztec2007@verizon.net>
2021-12-06 12:14:26 -0500

    worked on re-writing code.

--- script.js
+++ script.js
@@ -1,33 +1,53 @@
 // let playerSelection = prompt('Type your choice of rock, paper or scissors.');
 let playerSelection = 'rock';
 
-//let computerSelection = computerPlay();
-let computerSelection = computerPlay();
-
+playerSelection = playerSelection.toLowerCase();
 
 function computerPlay() {
   const rps = ['rock', 'paper', 'scissors'];
-let random = Math.floor(Math.random() * rps.length);
-return rps[random];
+
+  let random = Math.floor(Math.random() * rps.length);
+
+  return rps [random];
 }
 
 
+//let computerSelection = computerPlay();
+let computerSelection = computerPlay();
 
 function playGame(playerSelection, computerSelection) {
   if (playerSelection === computerSelection) {
     console.log("It's a tie");
+  } else if (
+    (computerSelection === 'rock' && playerSelection === 'scissors') ||
+    (computerSelection === 'scissors' && playerSelection === 'paper') ||
+    (computerSelection === 'paper' && playerSection === 'rock')
+  )
+}
 
-  else (computerSelection > playerSelection)
-  (
-  (computerSelection === 'rock' && playerSelection === 'scissors') ||
-  (computerSelection === 'scissors' && playerSelection === 'paper') ||
-  (computerSelection === 'paper' && playerSelection === 'rock')) 
+//function computerPlay() {
+ // const rps = ['rock', 'paper', 'scissors'];
+//let random = Math.floor(Math.random() * rps.length);
+//return rps[random];
+//}
+
+
+
+//function playGame(playerSelection, computerSelection) {
+ // if (playerSelection === computerSelection) {
+   // console.log("It's a tie");
+
+  //else (computerSelection > playerSelection)
+  //(
+  //(computerSelection === 'rock' && playerSelection === 'scissors') ||
+  //(computerSelection === 'scissors' && playerSelection === 'paper') ||
+  //(computerSelection === 'paper' && playerSelection === 'rock')) 
   
-console.log ("You loose!");
-} else if {
-console.log("You win! $(playerSelection"); beats $(computerSelection));
-}
-}
+//console.log ("You loose!");
+//} else if {
+//console.log("You win! $(playerSelection"); beats $(computerSelection));
+//}
+//}
 
 
 
