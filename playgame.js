9e19a8799a3b5d7084c51f9baaea6b63432bb3cd
MJZTEC2021 <mjztec2007@verizon.net>
2021-12-09 14:30:53 -0500

    Added playgame.js code

---
+++ playgame.js
@@ -0,0 +1,7 @@
+const changeText = () => {
+  const p = document.querySelector('p');
+
+  p.textContent = 'rock beats scissors.';
+};
+const rock = document.querySelector('rock');
+button.onclick = changeText;
--- script.js
+++ script.js
@@ -31,3 +31,12 @@ function playGame(playerSelection, computerSelection) {
   }
 }
 playGame(playerSelection, computerSelection);
+
+
+
+
+
+
+
+
+
