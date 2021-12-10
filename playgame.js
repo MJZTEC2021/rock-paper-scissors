76c3e637eb8a1836e4f74723d725f6e325f5e665
MJZTEC2021 <mjztec2007@verizon.net>
2021-12-09 19:54:41 -0500

    Worked with coding

--- playgame.js
+++ playgame.js
@@ -1,7 +1,21 @@
-const changeText = () => {
-  const p = document.querySelector('p');
+//const changeText = () => {
+// const p = document.querySelector('p');
 
-  p.textContent = 'rock beats scissors.';
+// p.textContent = 'rock beats scissors.';
+//};
+//const button = document.querySelector('button');
+//button.onclick = changeText;
+
+const p = document.querySelector('p');
+const button = document.querySelector('button');
+
+const changeText = () => {
+  p.textContent = 'Will I change?';
 };
-const button = document.querySelector('rock');
-button.onclick = changeText;
+
+const alertText = () => {
+  alert('Will I alert?');
+};
+
+button.addEventListener('click', changeText);
+button.addEventListener('click', alertText);
