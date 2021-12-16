87fba673e30671aede5b0f66c3a823423445fb99
MJZTEC2021 <mjztec2007@verizon.net>
2021-12-16 10:52:34 -0500

    deleted the extra .js file not needed

--- playgame.js
+++
@@ -1,9 +0,0 @@
-//const changeText = () => {
-// const p = document.querySelector('p');
-
-// p.textContent = 'rock beats scissors.';
-//};
-//const button = document.querySelector('button');
-//button.onclick = changeText;
-
-
